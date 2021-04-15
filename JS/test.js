5ff7c9d22fb3d2da1b83fb827efc2735d5de940a
ivelin <mihajlov.ivelin@gmail.com>
2021-04-15 17:37:05 +0300

    hovering over menu items changes background

--- JS/test.js
+++ JS/test.js
@@ -1,4 +1,18 @@
 function said()
 {
     console.log("Hi");
-}
+};
+
+const menuItem = document.querySelectorAll("a");
+
+function hover() {
+    let target = event.target;
+    target.style.background = 'pink';
+    target.style.color = 'black';
+};
+
+function leave() {
+    let target = event.target;
+    target.style.background = '';
+    target.style.color = 'white';
+};
